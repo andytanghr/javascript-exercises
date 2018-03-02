dbc31fa2570b04881d1b3175129a069be3150274
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-03-02 14:55:34 -0600

    first commit of blackjack

---
+++ scripts/sort3.js
@@ -0,0 +1,20 @@
+// Sort an array, 3
+var arr = [
+  [1, 3, 4],
+  [2, 4, 6, 8],
+  [3, 6]
+];
+
+var newArray = [];
+var sumList = [];
+
+function sumArray(arr) {
+    var sumList = arr.reduce(function(value, item) {
+
+    });
+}
+
+arr.sort(function(a, b) {
+  return (sumArray(a) - sumArray(b));
+});
+
