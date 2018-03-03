b67c2b902cb1c8cd3280a58c40b88930ab0a9d1a
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-03-02 21:48:13 -0600

    finished dynamicHighOnCoding page using JS functions to populate HTML

--- JSinjection/dynamicHighOnCoding.js
+++ JSinjection/dynamicHighOnCoding.js
@@ -1,84 +1,124 @@
-// (function() {
-//   function createHTMLHead(title) {
-//   // add boilerplate head, meta charset, etc.
-//   var lang = 'en'; //set html lang="en"
-  
-//   var titleTag = document.head.createElement('title');
-//   titleTag.textContent = 'okgo';
-//   document.head.appendChild(titleTag);
+(function () {
+  var html = document.getElementsByTagName('html')[0]; //0
+  var head = document.getElementsByTagName('head')[0]; //0
+  var body = document.getElementsByTagName('body')[0]; //0
+  var container = document.getElementById('container');
+  container.setAttribute('style', 'font-family: sans-serif');
 
-//   // add <title>
-//   }
+function createHtmlAndTitleTags() {
+  // var docType = document.createElement('!DOCTYPE');
+  // html.appendChild(docType);
 
-//   function createHeader(websiteTitle, home, otherNavs){
-//   // add 
+  html.setAttribute("lang", "en");
 
-//   }
+  var meta = document.createElement('meta');
+  meta.setAttribute('charset', 'UTF-8');
+  head.appendChild(meta);
 
-//   function createCurrentArticle(title, body) {
+  meta = document.createElement('meta');
+  meta.setAttribute('name', 'viewport');
+  meta.setAttribute('content', 'width=device-width, initial-scale=1.0');
+  head.appendChild(meta);
 
-//   }
+  meta = document.createElement('meta');
+  meta.setAttribute('http-equiv', 'X-UA-Compatible');
+  meta.setAttribute('content', 'ie=edge');
+  head.appendChild(meta);
 
-//   function createOlderArticle(title, body) {
+  var titleTag = document.createElement('title'); //1
+  titleTag.textContent = 'HighOnCoding';  //2
+  head.appendChild(titleTag); //3
+}
 
-//   }
+  function createHeader() {
+    var header = document.createElement('header');
+    header.setAttribute('style', 'background: rgb(101, 167, 243); color: white; padding: 36px 36px 28px 26px;');
+    container.appendChild(header);
+    
+    var h1Website = document.createElement('h1');
+    h1Website.setAttribute('style', 'display: inline; margin-right: 64px; font-size: 2.2em;');
+    h1Website.textContent = 'HighOnCoding';
+    header.appendChild(h1Website);
 
-//   function createReactions(comments, likes) {
+    var h2Home = document.createElement('h2');
+    h2Home.setAttribute('style', 'display: inline; margin-right: 48px;');
+    var strong = document.createElement('strong');
+    strong.textContent = 'Home';
+    h2Home.appendChild(strong);
+    header.appendChild(h2Home);
 
-//   }
+    var h2Categories = document.createElement('h2');
+    h2Categories.setAttribute('style', 'display: inline; font-weight: 200;');
+    h2Categories.textContent = 'Categories';
+    header.appendChild(h2Categories);
 
+    var br = document.createElement('br');
+    container.appendChild(br);
+  }
 
-//   var websiteTitle = 'HighOnCoding';
-//   var home = 'Home';
-//   var otherNavs = 'Categories'; // could expand nav links with an array
+  function createCurrentArticle(title, body, hash) {
+    var article = document.createElement('article');
+    article.setAttribute('class', hash);
+    article.setAttribute('style', 'background: rgb(221, 222, 224); margin: 6px 29px 0 29px; padding: 4px 16px;');
+    container.appendChild(article);
 
-//   /*
-//   var currentArticleTitle = 'Curse of the Current Reviews';
-//   var currentArticleBody = 'When you want to buy any application from the Apple iTunes store you have more choices than you can handle. Most of the users scroll past the application description completely avoiding it like ads displayed on the right of the column of your webpage. Their choice is dependent on three important factors price, screenshot and reviews.';
+    var h1 = document.createElement('h1');
+    h1.setAttribute('style', 'color: rgb(84, 88, 94); font-size: 1.88em;');
+    h1.textContent = title;
+    article.appendChild(h1);
+    
+    var p = document.createElement('p');
+    p.setAttribute('style', 'font-size: 0.86em; line-height: 1.2em; padding-top: 12px;');
+    p.textContent = body;
+    article.appendChild(p);
+  }
 
-//   var olderArticleTitle0 = 'Hello WatchKit';
-//   var olderArticleBody0 = 'Last month Apple released the anticipated WatchKit Framework for developers in the form of iOS 8.2 beta SDK release. The WatchKit framework enable the developers to create Apple Watch applications. In this article we are going to focus on the basics of getting started with the WatchKit framework and developing apps for the Apple Watch.';
-//   var olderArticleComments0 = 12;
-//   var olderArticleLikes0 = 124;
+  function createOlderArticle(title, body, hash, comments, likes) {
+    var article = document.createElement('article');
+    article.setAttribute('class', hash);
+    article.setAttribute('style', 'margin-left: 34px; margin-right: 37px;');
+    container.appendChild(article);
 
-//   var olderArticleTitle1 = 'Introduction to Swift'
-//   var olderArticleBody1 = 'Swift is a modern programming language developed by Apple to create the next generation of iOS and OSX applications. Swift is a fairly new language and still in development but it clearly reflects the intentions and the future direction. This article will revolve around the basic concepts in the Swift language and how you can get started.';
-//   var olderArticleComments1 = 15;
-//   var olderArticleLikes1 = 45;
+    var h2 = document.createElement('h2');
+    h2.setAttribute('style', 'color: rgb(38, 102, 186); font-weight: 500;');
+    h2.textContent = title;
+    article.appendChild(h2);
+    
+    var p = document.createElement('p');
+    p.setAttribute('style', 'font-size: 0.86em; line-height: 1.2em;');
+    p.textContent = body;
+    article.appendChild(p);
 
-//   createHTMLHead(websiteTitle);
-//   createHeader(websiteTitle, home, otherNavs);
-//   createCurrentArticle(currentArticleTitle, currentArticleBody);
-//   createOlderArticle(olderArticleTitle0, olderArticleBody0);
-//   createOlderArticle(olderArticleTitle1, olderArticleBody1);
-//   */
+    var div = document.createElement('div');
+    div.setAttribute('class', hash);
+    div.setAttribute('style', 'background: rgb(230, 148, 59); color: white; padding: 6px; font-weight: 500;');
+    article.appendChild(div);
 
+    var pComments = document.createElement('p');
+    pComments.setAttribute('style', 'display: inline; margin-right: 27px;');
+    pComments.textContent = comments + ' comments';
+    div.appendChild(pComments);
 
-//   var titleTag = document.head.createElement('title');
-//   titleTag.innerHTML = 'okgo';
-//   document.head.appendChild(titleTag);
+    var pLikes = document.createElement('p');
+    pLikes.setAttribute('style', 'display: inline;');
+    pLikes.textContent = likes + ' likes';
+    div.appendChild(pLikes);
 
-//   var test = document.body.createElement('p');
-//   test.innerHTML = 'test innerHTML';
+    var br = document.createElement('br');
+    container.appendChild(br);
+  }
 
+createHtmlAndTitleTags();
+createHeader();
+createCurrentArticle('Curse of the Current Reviews', 'When you want to buy any application from the Apple iTunes store you have more choices than you can handle. Most of the users scroll past the application description completely avoiding it like ads displayed on the right of the column of your webpage. Their choice is dependent on three important factors price, screenshot and reviews.', 'd4595b');
+createOlderArticle('Hello WatchKit', 'Last month Apple released the anticipated WatchKit Framework for developers in the form of iOS 8.2 beta SDK release. The WatchKit framework enable the developers to create Apple Watch applications. In this article we are going to focus on the basics of getting started with the WatchKit framework and developing apps for the Apple Watch.', '0c11b6', 12, 124);
+createOlderArticle('Introduction to Swift', 'Swift is a modern programming language developed by Apple to create the next generation of iOS and OSX applications. Swift is a fairly new language and still in development but it clearly reflects the intentions and the future direction. This article will revolve around the basic concepts in the Swift language and how you can get started.', '01b0ce', 15, 45);
 
+//create main html element node, like html, head, body
+//create element node
+//create text node, using textcontent
+//append text node to parent element node
+//append element to higher parent node like in a div
 
-
-//   var docType = document.createElement('DOCTYPE'); // 'html';// <!DOCTYPE html>
-//   docType.innerHTML = 'html';
-
-
-//   var element1 = document.createElement('p');
-//   element1.innerHTML = 'hello';
-
-//   document.body.innerHTML = 'en'; //set html lang="en"
-
-// })();
-
-(function() {
-  var titleTag = document.createElement('p');
-  var titleText = titleTag.createTextNode = 'okgo';
-  titleTag.appendChild(titleText);
-  var body = document.getElementsByTagName('body')[0];
-  document.body.appendChild(titleTag);
 })();
+
